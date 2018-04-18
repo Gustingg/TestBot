fe3a25580e8c233015a4333be658f5ff031be7ff
Gust <gustavo.vilas@yahoo.com.br>
2018-04-18 19:55:24 -0300

    StarT

---
+++ TestBot/Dialogs/RootDialog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace TestBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            var message = activity.CreateReply();
+
+            var heroCard = new HeroCard();
+            heroCard.Title = "Bem vindo!";
+            heroCard.Text = "Sou eu, seu Pé de Feijão. Gostaria de saber a temperatura do ar? Digite !temp caso queira saber a umidade do ar? Digite !humiar ou então está interessado saber a umidade do solo, então digite !humisol";
+
+            message.Attachments.Add(heroCard.ToAttachment());
+
+            await context.PostAsync(message);
+
+            // calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            Random x = new Random();
+            int Temp = x.Next(-10, 40);
+            if (Temp <= 20 && Temp >= 10)
+            {
+                await context.PostAsync($"A temperatura está muito agradável! \n Temperatura atual: "+Temp);
+            }
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ TestBot/Dialogs/TemperaturaDialog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace TestBot.Dialogs
+{
+    [Serializable]
+    [LuisModel("e295f99b-4260-488b-91bb-c09bd2e641d6", "a57e46d658dc4fb99d94b2123acaed5b")]
+
+    public class TemperaturaDialog : LuisDialog<object>
+    {
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Desculpe, não entendi, poderia falar novamente?");
+        }
+        [LuisIntent("Sobre")]
+        public async Task Sobre(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Eu sou sua planta! Estou aqui pra ajudar a você cuidar de mim. :D");
+        }
+        [LuisIntent("Cumprimento")]
+        public async Task Cumprimento(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Olá! Caso queira saber algo sobre mim é só perguntar! :)");
+        }
+        [LuisIntent("Temperatura")]
+        public async Task Temperatura(IDialogContext context, LuisResult result)
+        {
+            var temp = result.Entities?.Select(e => e.Type);
+            Random x = new Random();
+            int Temp = x.Next(-10, 40);
+            await context.PostAsync($"A temperatura está muito agradável! \n Temperatura atual: " + Temp);
+        }
+    }
+}
